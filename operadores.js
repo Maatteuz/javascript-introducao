e7dbcf7393f23fcafa7016937216bdeff199db32
Mateus Lima <mateus9911@hotmail.com>
2025-06-16 11:23:56 -0300

    i

---
+++ operadores.js
@@ -0,0 +1,66 @@
+//  operadores aritméticos
+
+// console.log("2 + 2 = " , 2 + 2);
+// console.log("3 - 2 = " , 3 - 2);
+// console.log("2 * 6 = " , 2 * 6);
+// console.log("10 / 5 = " , 10 / 5);
+// console.log("10 % 5 = " , 10 % 5);
+
+
+
+
+// operadores atribuidores
+
+// let numero = 0;
+// console.log("numero: " , numero);
+// numero = 2; // reatribui um valor
+// console.log("atribui: " , numero);
+// numero += 3; // soma 3 ao valor que ja estiver na variável
+// console.log("somei 3: " , numero);
+// numero -= 2; // subtrai 2 ao valor que tem na variável 
+// console.log("subtrai 2: " , numero);
+// numero *=4; // multiplica por 4 o que já tem na variável
+// console.log("multipliquei por 4: " , numero);
+// numero /=2; // divide por 2 o que estiver na variável
+// console.log("dividi por 2: " , numero);
+
+
+// operadores condicionais 
+
+// console.log("2 > 1: ", 2 > 1);
+// console.log("2 < 1: ", 2 < 1);
+// console.log("2 >= 2: ", 2 >= 2);
+// console.log("2 <= 3: ", 2 <= 3);
+// console.log("1 == 1: ", 1 == 1);
+// console.log("Vitoria == vitoria: " , 'Vitoria' == 'vitoria');
+// let senha = 1234
+// console.log("Senha: ", senha == 1234);
+// console.log("Senha: ", senha != 12345);
+// console.log("2 == 2: ", '2' == 2);
+// console.log("2 != 2: ", '2' != 2);
+// console.log("2 === 2: ", '2' === 2);
+// console.log("2 !== 2: ", '2' !== 2);
+
+
+
+// operadores lógicos
+
+// console.log("2 && true: " , 2 && true);
+// console.log("2 == 2 && 3 == 3: " , 2 == 2 && 3 == 3);
+// console.log("2 == 3 || 3 == 3: " , 2 == 3 || 3 == 3);
+// console.log("2 > 3 || 3 % 2 == 0: " , 2 > 3 % 2 == 0);
+// console.log("!true: " , !true);
+// console.log("!false: " , !false);
+// let idade;
+// if(!idade){
+//     console.log("Idade deve ser declarada!");
+//     }
+
+
+
+
+
+
+
+
+
