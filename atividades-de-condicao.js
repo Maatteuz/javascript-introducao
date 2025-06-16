e6b14f9fcf7319c3095da907c77c72e04558910d
Mateus Lima <mateus9911@hotmail.com>
2025-06-16 11:47:33 -0300

    att2

--- atividades-de-condicao.js
+++ atividades-de-condicao.js
@@ -1,7 +1,23 @@
-let idade = 18;
-if(idade >= 18) {
-    console.log("Maior de idade");
-}else{
-    console.log("Menor de idade");
+// let idade = 17;
+// if(idade >= 18) {
+//     console.log("Maior de idade");
+// }else{
+//     console.log("Menor de idade");
     
+// }
+
+// questão 2
+let numero;
+
+if(!numero){
+    console.log("Digite um numero válido");
+    return;
+    }
+
+if(numero > 0){
+    console.log(`${numero} é positivo`);
+}else if(numero < 0){
+    console.log(`${numero} é negativo`);
+}else{
+    console.log(`é zero`);
 }
