7fa03614fa0b59d8943a7b655af4e48381cbcd11
Mateus Lima <mateus9911@hotmail.com>
2025-06-23 09:50:47 -0300

    llll

--- atividades-de-condicao.js
+++ atividades-de-condicao.js
@@ -7,17 +7,123 @@
 // }
 
 // questão 2
-let numero;
+// let numero;
 
-if(!numero){
-    console.log("Digite um numero válido");
-    return;
-    }
+// if(!numero){
+//     console.log("Digite um numero válido");
+//     return;
+//     }
 
-if(numero > 0){
-    console.log(`${numero} é positivo`);
-}else if(numero < 0){
-    console.log(`${numero} é negativo`);
-}else{
-    console.log(`é zero`);
+// if(numero > 0){
+//     console.log(`${numero} é positivo`);
+// }else if(numero < 0){
+//     console.log(`${numero} é negativo`);
+// }else{
+//     console.log(`é zero`);
+// }
+
+// questão 3
+
+// let numeroA = 3;
+// let numeroB = 8;
+
+// if(numeroA > numeroB){
+//     console.log(`${numeroA} é maior do que numeroB`)
+// }else if(numeroB > numeroA){
+//     console.log(`${numeroB} é maior que ${numeroA}`);
+// }else{
+//     console.log(`${numeroA} é igual ${numeroB}`);
+
+// questão 4
+
+// let numero = 3;
+
+// if(numero % 2 === 0){
+//     console.log(`${numero} é par`);
+// }else{
+//     console.log(`${numero} é impar`);
+// }
+
+// questão 5
+
+// let notaA = 10;
+// let notaB = 10;
+// let notaC = 10;
+// let media = ((notaA + notaB + notaC) / 3).toFixed(1);
+
+// if(media >= 7){
+//     console.log(`Média: ${media}, Parabéns você foi aprovado`);
+// }else if(media >= 5){
+//     console.log(`Média: ${media}, Você esta de recuperação , estude mais!`);
+// }else{
+//     console.log(`Média: ${media}, Você foi reprovado!`);
+// }
+
+// questão 6
+
+// let valor = 90;
+
+// if(valor > 100){
+//     console.log(`Você recebeu um desconto de 10%, total:R$ ${(valor - (valor *0.1)).toFixed(2)}`);   
+// }else{
+//     console.log(`Valor a pagar: R$ ${(valor).toFixed(2)}`);
+    
+// }
+
+// questão 7 = professor pulou 
+
+// questão 8 
+
+// let usuario = "admin";
+// let senha = "1234";
+
+// if(usuario === "admin" && senha === "1234"){
+//     console.log("Acesso permitido");
+// }else{
+//     console.log("Acesso Negado");
+// }
+
+// questão 9
+
+// let compra = 100;
+
+// if(compra >= 200){
+//     console.log(`Você obteve frete grátis em sua compra!; total: R$ ${compra}`);
+// }else{
+//     console.log(`Total da compra + frete: R$ ${(compra + 20).toFixed(2)}`);
+// } 
+
+
+// questão 10
+
+// let numero = 125;
+
+// if( numero >= 10 && numero <= 50){
+//     console.log(`O numero ${numero} esta entre 10 e 50`);
+// }else{
+//     console.log(`O numero ${numero} não esta entre 10 e 50`);
+// }
+
+
+// questão 1 -> switch case
+
+let dia = 3;
+
+switch (dia) {
+    case "verde":
+        console.log("Siga em frente");
+        break;
+    case "amarelo":
+        console.log("Atenção");
+        break;
+    case "verde":
+        console.log("PARE");
+        break;
+    default:
+        console.log("Informe um valor valido : verde , amarelo ou vermelho");
+        break;
 }
+
+
+
+
